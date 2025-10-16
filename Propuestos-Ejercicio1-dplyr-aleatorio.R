ccce2f1026ed9c110f2c899fe5763b6e79956a4d
Sebastián Cabezas Ríos <68340396+scabezas-academico@users.noreply.github.com>
2025-10-16 10:49:02 -0300

    Create Propuestos-Ejercicio1-dplyr-aleatorio.R

---
+++ Propuestos-Ejercicio1-dplyr-aleatorio.R
@@ -0,0 +1,26 @@
+library(dplyr)
+# DATOS DE LECTURAS DE SENSORES EN BOMBAS DE EXTRACCIÓN
+registros_bomba <- data.frame(
+  id_bomba = c("B-01", "B-02", "B-03", "B-04", "B-05", "B-06", "B-07", "B-08"),
+  tiempo_uso_hrs = c(150, 210, 85, 305, 120, 190, 250, 95),
+  temperatura_max_c = sample(x = 75:105, 8),
+  ubicacion = c("Norte", "Sur", "Norte", "Oeste", "Sur", "Oeste", "Norte", "Sur")
+);
+
+# Definir la temperatura crítica máxima
+TEMP_CRITICA <- 90;
+
+print(registros_bomba);
+
+# datos
+bombas_criticas <- registros_bomba %>%
+  # 1 Filtrar: por las bombas que tienen temperatura mayor al umbral
+  filter(temperatura_max_c >= TEMP_CRITICA) %>%
+  # 2 Seleccionar: solo las columnas relevantes
+  select(id_bomba, temperatura_max_c, ubicacion) %>%
+  # 3 Nueva Columna: Alarma
+  mutate(estado = "Alarma!") %>%
+  # 4 Ordenar: atentiendo la más crítica primero
+  arrange(desc(temperatura_max_c))
+
+print(bombas_criticas);
